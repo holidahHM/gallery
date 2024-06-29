b6ddd2d8e9654187a547fe7dff3a7dd8219d6809
holidahHM <holidahmwangi@gmail.com>
2024-06-29 12:30:08 +0300

    Updated Config file to the user credentials

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://holidahmwangi:lWaSaWC1RJAdT4Ej@tmcluster.zbhbjtc.mongodb.net/?retryWrites=true&w=majority&appName=TMCluster',
-    development: 'mongodb+srv://holidahmwangi:lWaSaWC1RJAdT4Ej@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://holidahmwangi:lWaSaWC1RJAdT4Ej@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://holliemwangi:H0llies@m@tmcluster.zbhbjtc.mongodb.net/?retryWrites=true&w=majority&appName=TMCluster',
+    development: 'mongodb+srv://holliehmwangi:H0llies@m@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://holliemwangi:H0llies@m@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
