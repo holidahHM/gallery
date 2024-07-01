57de4208f82e15694990f93ef05547adfe1bcec6
holidahHM <holidahmwangi.com>
2024-07-01 12:34:13 +0300

    updated env file

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://holliemwangi:H0llies@m@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    production: 'mongodb+srv://holliemwangi:H0llies@m@tmcluster.zbhbjtc.mongodb.net/?retryWrites=true&w=majority&appName=TMCluster',
     development: 'mongodb+srv://holliemwangi:H0llies@m@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://holliemwangi:H0llies@m@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
