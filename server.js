c8acad5e411a51f066c4b7a8dd2e1e786a458daa
holidahHM <holidahmwangi.com>
2024-07-01 10:27:18 +0300

    updated db connection on .env file

--- server.js
+++ server.js
@@ -1,3 +1,51 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const path = require('path');
+
+// // Define routes
+// let index = require('./routes/index');
+// let image = require('./routes/image');
+
+// // connecting the database
+// let mongodb_url = 'mongodb://localhost:27017/';
+// let dbName = 'darkroom';
+// mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
+//     if (err) console.log(err)
+// });
+
+// // test if the database has connected successfully
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
+
+// // Initializing the app
+// const app = express();
+
+
+// // View Engine
+// app.set('view engine', 'ejs');
+
+// // Set up the public folder;
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // body parser middleware
+// app.use(express.json())
+
+
+// app.use('/', index);
+// app.use('/image', image);
+
+
+
+ 
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT,() =>{
+//     console.log(`Server is listening at http://localhost:${PORT}`)
+// });
+
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,40 +55,39 @@ const path = require('path');
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
+// MongoDB connection string from environment variable
+//const mongodb_url = process.env.MONGO_URI || 'mongodb://localhost:27017/darkroom';
+const mongodb_url = process.env.MONGO_URI || 'mongodb://mongodb_ip_address:27017/darkroom';
+
+
+// Connect to the database
+mongoose.connect(mongodb_url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) console.log(err);
 });
 
-// test if the database has connected successfully
+// Test if the database has connected successfully
 let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+db.once('open', () => {
+  console.log('Database connected successfully');
+});
 
-// Initializing the app
+// Initialize the app
 const app = express();
-
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
-app.use(express.json())
+// Body parser middleware
+app.use(express.json());
 
-
+// Use routes
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
