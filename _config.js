c48f1aa6f9a5db6973811df9fb96dadc700af53a
holidahHM <holidahmwangi.com>
2024-07-01 12:38:22 +0300

    updated env file

--- _config.js
+++ _config.js
@@ -4,7 +4,7 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://holliemwangi:H0llies@m@tmcluster.zbhbjtc.mongodb.net/?retryWrites=true&w=majority&appName=TMCluster',
     development: 'mongodb+srv://holliemwangi:H0llies@m@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://holliemwangi:H0llies@m@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://holliemwangi:H0llies@m@tmcluster.zbhbjtc.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
 
